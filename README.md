f479834c8b05cb10f71f1852da7663c0a0cb8437
JP Meijers <jp.meijers@canonical.com>
2026-02-28 18:02:01 +0200

    Snap open-webui python package (#2)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# open-webui-snap
+# Open WebUI snap
+
+This repository contains the snap packaging configurations for Open WebUI.
+
+Open WebUI itself is included into the snap as a Python pip package.
---
+++ scripts/info.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+HOST=$(snapctl get host)
+PORT=$(snapctl get port)
+
+SERVICE_NAME="$SNAP_INSTANCE_NAME.server"
+SERVICE_INFO=$(snapctl services $SERVICE_NAME)
+
+STATUS=$(echo "$SERVICE_INFO" | awk -v svc="$SERVICE_NAME" '$0 ~ svc {print $3}')
+
+echo "$SERVICE_NAME: $STATUS"
+
+if [ "$STATUS" == "active" ]; then
+  echo ''
+  echo "To use $SNAP_INSTANCE_NAME, open the following URL in your browser: http://$HOST:$PORT"
+else
+  echo ''
+  echo "If you expected the service to be up, check that the port $PORT is available, not bound by some other service."
+fi
